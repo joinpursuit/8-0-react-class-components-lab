d2fd78c1f5a23805adcfd3ac99a5c459ff9452d7
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-10-29 19:03:22 -0400

    Class components labs done

--- src/Post.js
+++ src/Post.js
@@ -7,14 +7,23 @@ const postInfo = {
   description: "This is the description of the post",
 };
 
-const Post = () => {
-  return (
-    <div className="Post" data-testid="post">
-      <p>{postInfo.title}</p>
-      <img src={postInfo.imageLink} alt="post" />
-      <p>{postInfo.description}</p>
-    </div>
-  );
-};
+class Post extends React.Component {
+  constructor(){
+    super();
+    this.postInfo=postInfo;
+    
+    
+  }
+  render() {
+    return (
+      <div className="Post" data-testid="post">
+        <p>{this.postInfo.title}</p>
+        <img src={this.postInfo.imageLink} alt="post" />
+        <p>{this.postInfo.description}</p>
+      </div>
+    );
+
+  }
+}
 
 export default Post;
