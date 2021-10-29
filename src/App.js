2c94734e72c7f3d0637a70b729ae2b35cd306c5b
Nima sherpa <Toplouie78@gmail.com>
2021-10-29 18:17:07 -0400

    Test Completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App-Div">
-      <Feed />
-      <ContactList />
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App-Div">
+				<Feed />
+				<ContactList />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Feed.js
+++ src/Feed.js
@@ -2,19 +2,22 @@ import React from "react";
 import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
+import { Component } from "react";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
-      <h2>Feed</h2>
-      <Post />
-      <Post />
-      <Ad />
-      <Post />
-      <Ad />
-      <Post />
-    </div>
-  );
-};
+class Feed extends Component {
+	render() {
+		return (
+			<div className="Feed" data-testid="feed">
+				<h2>Feed</h2>
+				<Post />
+				<Post />
+				<Ad />
+				<Post />
+				<Ad />
+				<Post />
+			</div>
+		);
+	}
+}
 
 export default Feed;
