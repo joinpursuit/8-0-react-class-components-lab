a8965b92e8df789a08a9b5413b7eb1c5f24ef8a0
Lakisha J <lakishajohnson@pursuit.org>
2021-10-30 16:29:54 -0400

    Post refractored

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Post.css";
 
-const postInfo = {
+/*const postInfo = {
   title: "Sample Post Title",
   imageLink: "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg",
   description: "This is the description of the post",
@@ -15,6 +15,25 @@ const Post = () => {
       <p>{postInfo.description}</p>
     </div>
   );
-};
+};*/
+class Post extends React.Component {
+  constructor() {
+    super() 
+      this.postInfo = {
+        title: "Sample Post Title",
+        imageLink: "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg",
+        description: "This is the description of the post",
+      };
+    }
+    render() {
+      return (
+        <div className="Post" data-testid="post">
+      <p>{this.postInfo.title}</p>
+      <img src={this.postInfo.imageLink} alt="post" />
+      <p>{this.postInfo.description}</p>
+    </div>
+      )
+    }
+  }
 
 export default Post;
