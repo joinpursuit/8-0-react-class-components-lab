2240e9989467a8498573d71c96a7822d0e6f9ed0
Angie G <angieg@Angies-MBP.fios-router.home>
2021-10-30 16:37:46 -0400

    ContactList.js File Refactoring

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./ContactList.css";
 
-
+class ContactList extends React.Component {
+    //const ContactList = () => {
+      render() {
+        return (
+          <div className="ContactList" data-testid="contact-list">
+            <p>Contacts</p>
+            <ul>
+              <li>Andrew Clark</li>
+              <li>Brian Vaughn</li>
+              <li>Dan Abramov</li>
+              <li>Flarnie Marchan</li>
+            </ul>
+          </div>
+      );
+    };
+    };
+    
 
 export default ContactList;
