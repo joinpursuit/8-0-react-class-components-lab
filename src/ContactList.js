af4dccc5593e69018f0c58e347e2a412558d46e1
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2021-10-29 22:11:33 -0400

    Lab Completed

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,18 +1,36 @@
-import React from "react";
+import { Component } from "react";
 import "./ContactList.css";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
-      <p>Contacts</p>
-      <ul>
-        <li>Andrew Clark</li>
-        <li>Brian Vaughn</li>
-        <li>Dan Abramov</li>
-        <li>Flarnie Marchan</li>
-      </ul>
-    </div>
-  );
-};
+// const ContactList = () => {
+//   return (
+//     <div className="ContactList" data-testid="contact-list">
+//       <p>Contacts</p>
+//       <ul>
+//         <li>Andrew Clark</li>
+//         <li>Brian Vaughn</li>
+//         <li>Dan Abramov</li>
+//         <li>Flarnie Marchan</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+class ContactList extends Component{
+  constructor(){
+    super();
+    this.contacts = ["Andrew Clark", "Brian Vaughn", "Dan Abramov", "Flarnie Marchan"];
+  }
+  render(){
+    return(
+      <div className="ContactList" data-testid="contact-list">
+        <p>Contacts</p>
+        {this.contacts.map((contact) => 
+          <ul key={contact}>
+            <li>{contact}</li>
+          </ul>)}
+      </div>
+    );
+  };
+}
 
 export default ContactList;
