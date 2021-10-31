357067fc0c507c5382ce69d9e8cd1a35a6b84b5e
Jalamang <Jalamangjanka@gmail.com>
2021-10-30 23:30:48 -0400

    Modified profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './Profile.css'
+import "./Profile.css";
 class Profile extends React.Component {
   constructor() {
     super();
     this.text = "Biography of Nelson Mandela";
-    this.bio = "Nelson Rolihlahla Mandela was born in Transkei, South Africa on July 18, 1918. His father was Hendry Mphakanyiswa of the Tembu Tribe. Mandela himself was educated at University College of Fort Hare and the University of Witwatersrand where he studied law. He joined the African National Congress in 1944 and was engaged in resistance against the ruling National Party’s apartheid policies after 1948. He went on trial for treason in 1956-1961 and was acquitted in 1961.";
+    this.bio =
+      "Nelson Rolihlahla Mandela was born in Transkei, South Africa on July 18, 1918. His father was Hendry Mphakanyiswa of the Tembu Tribe. Mandela himself was educated at University College of Fort Hare and the University of Witwatersrand where he studied law. He joined the African National Congress in 1944 and was engaged in resistance against the ruling National Party’s apartheid policies after 1948. He went on trial for treason in 1956-1961 and was acquitted in 1961.";
     this.image =
       "https://www.nobelprize.org/images/mandela-13452-content-portrait-mobile-tiny.jpg";
   }
@@ -12,16 +13,24 @@ class Profile extends React.Component {
     return (
       <div className="profile" data-testid="Profile">
         <h1>Profile</h1>
-          <h2 className = 'text'>{this.text}</h2>    
-        <div className = 'profile-grid'>
-            <div>
+        <h2 className="text">{this.text}</h2>
+        <div className="profile-grid">
+          <div>
             <img src={this.image} />
           </div>
           <div>
-            <p >{this.bio} <a href="https://www.nobelprize.org/prizes/peace/1993/mandela/biographical/" class="btn" target = '_blank'>Read More</a>
-           </p>
+            <p>
+              {this.bio}{" "}
+              <a
+                href="https://www.nobelprize.org/prizes/peace/1993/mandela/biographical/"
+                class="btn"
+                target="_blank"
+              >
+                Read More
+              </a>
+            </p>
           </div>
-        </div>  
+        </div>
       </div>
     );
   }
