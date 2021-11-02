714156262c7d5b2823078cbf67f593c35c4f210f
Jerry De Jesus <jdejesus7@live.com>
2021-11-02 12:53:28 -0400

    completed lab

--- src/Feed.js
+++ src/Feed.js
@@ -3,9 +3,10 @@ import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
+class Feed extends React.Component{
+  render(){
+    return (
+      <div className="Feed" data-testid="feed">
       <h2>Feed</h2>
       <Post />
       <Post />
@@ -14,7 +15,8 @@ const Feed = () => {
       <Ad />
       <Post />
     </div>
-  );
-};
+    )
+  }
+}
 
 export default Feed;
