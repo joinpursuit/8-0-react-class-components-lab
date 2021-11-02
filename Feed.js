cdd02545c957d0bf3964f2d909324e5882cf605c
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2021-11-02 17:27:59 -0400

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Ad from "./Ad.js";
+import Post from "./Post.js";
+import "./Feed.css";
+
+class Feed extends React.Component {
+  render() {
+  return (
+    <div className="Feed" data-testid="feed">
+      <h2>Feed</h2>
+      <Post />
+      <Post />
+      <Ad />
+      <Post />
+      <Ad />
+      <Post />
+    </div>
+  );
+  }
+};
+
+export default Feed;
