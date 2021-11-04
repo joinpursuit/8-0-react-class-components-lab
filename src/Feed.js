f787f77793411c62f12f855e066d2247e9e90a89
Milton Clark <kingcoder247@gmail.com>
2021-11-04 03:49:48 -0400

    Change - refactored Feed.js to a class component

--- src/Feed.js
+++ src/Feed.js
@@ -2,19 +2,26 @@ import React from "react";
 import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
+import { Component } from "react";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
-      <h2>Feed</h2>
-      <Post />
-      <Post />
-      <Ad />
-      <Post />
-      <Ad />
-      <Post />
-    </div>
-  );
+class Feed extends Component {
+    constructor(){
+      super()
+    };
+
+    render(){
+      return (
+        <div className="Feed" data-testid="feed">
+          <h2>Feed</h2>
+          <Post />
+          <Post />
+          <Ad />
+          <Post />
+          <Ad />
+          <Post />
+        </div>
+      );
+    };
 };
 
 export default Feed;
