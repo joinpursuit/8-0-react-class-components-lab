53e65b1de9abcd7170ef6c55c8c9dae6d74f958c
ziqian pan <ziqianpan1998@gmail.com>
2022-05-08 14:14:36 -0400

    finished lab

--- src/Feed.js
+++ src/Feed.js
@@ -3,8 +3,26 @@ import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-const Feed = () => {
-  return (
+// const Feed = () => {
+//   return (
+//     <div className="Feed" data-testid="feed">
+//       <h2>Feed</h2>
+//       <Post />
+//       <Post />
+//       <Ad />
+//       <Post />
+//       <Ad />
+//       <Post />
+//     </div>
+//   );
+// };
+
+class Feed extends React.Component {
+  // constructor() {
+  //   super()
+  // }
+  render() {
+      return (
     <div className="Feed" data-testid="feed">
       <h2>Feed</h2>
       <Post />
@@ -15,6 +33,10 @@ const Feed = () => {
       <Post />
     </div>
   );
-};
+  }
+
+}
+
+
 
 export default Feed;
