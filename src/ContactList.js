f6196670fbd08ae25f9f9531e5286d55327d1910
Mahajaya <mahajaya@Mahajayas-MacBook-Air.local>
2022-05-08 17:55:22 -0400

    React-Class-Components

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./ContactList.css";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
+class ContactList extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div className="ContactList" data-testid="contact-list">
       <p>Contacts</p>
       <ul>
         <li>Andrew Clark</li>
@@ -12,7 +17,24 @@ const ContactList = () => {
         <li>Flarnie Marchan</li>
       </ul>
     </div>
-  );
+    )
+  }
 };
 
 export default ContactList;
+
+// const ContactList = () => {
+//   return (
+//     <div className="ContactList" data-testid="contact-list">
+//       <p>Contacts</p>
+//       <ul>
+//         <li>Andrew Clark</li>
+//         <li>Brian Vaughn</li>
+//         <li>Dan Abramov</li>
+//         <li>Flarnie Marchan</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ContactList;
