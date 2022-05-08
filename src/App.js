6fb14b1ef75dacc6aa8796e0a53d05da0f832e18
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-05-08 13:14:12 -0400

    lab done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
 
-function App() {
-  return (
-    <div className="App-Div">
+class App extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render () {
+    return (
+      <div className="App-Div">
       <Feed />
       <ContactList />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App-Div">
+//       <Feed />
+//       <ContactList />
+//     </div>
+//   );
+// }
+
+// export default App;
