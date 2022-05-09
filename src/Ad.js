bf15e172525cc672daa1a32b3ed39b623b634c89
Stephanie Frias <steph.frias01@me.com>
2022-05-09 16:27:39 -0400

    completed lab

--- src/Ad.js
+++ src/Ad.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, Component } from "react";
 import "./Ad.css";
 
 export const adInfo = [
@@ -15,17 +15,21 @@ export const adInfo = [
     text: "Celebs: Where are they now??",
   },
 ];
+class Ad extends Component {
+  constructor() {
+    super();
+    this.index = Math.floor(Math.random() * adInfo.length);
+    this.ad = adInfo[this.index];
+  }
 
-const Ad = () => {
-  const index = Math.floor(Math.random() * adInfo.length);
-  const ad = adInfo[index];
-
-  return (
-    <div className="Ad" data-testid="ad">
-      <img src={ad.src} alt="cool ad" />
-      <p data-testid="ad-text">{ad.text}</p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="Ad" data-testid="ad">
+        <img src={this.ad.src} alt="cool ad" />
+        <p data-testid="ad-text">{this.ad.text}</p>
+      </div>
+    );
+  }
+}
 
 export default Ad;
