d1da62c2d50e23b37788d1c159aea7edaef73459
Andres Varon <anvaron@gmail.com>
2022-05-09 06:28:44 -0400

    <ContactList /> › has been refactored as a class component - tested

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./ContactList.css";
 
-//const ContactList = () => {
+// const ContactList = () => {
+//   return (
+//     <div className="ContactList" data-testid="contact-list">
+//       <p>Contacts</p>
+//       <ul>
+//         <li>Andrew Clark</li>
+//         <li>Brian Vaughn</li>
+//         <li>Dan Abramov</li>
+//         <li>Flarnie Marchan</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+
 class ContactList extends React.Component {
   constructor() {
     super()
@@ -21,8 +35,5 @@ class ContactList extends React.Component {
     );
   }
 }
-
- 
-
 
 export default ContactList;
