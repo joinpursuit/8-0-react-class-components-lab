cc6160541de3a917d5bdb5a2f846bc5d3b634ea6
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-05-13 21:10:35 -0400

    First and final commit.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Component } from "react";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
 
-function App() {
-  return (
-    <div className="App-Div">
-      <Feed />
-      <ContactList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+			<div className="App-Div">
+				<Feed />
+				<ContactList />
+			</div>
+		);
+  }
 }
 
 export default App;
